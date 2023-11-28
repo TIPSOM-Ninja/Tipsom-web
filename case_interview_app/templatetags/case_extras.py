89c3b7bedaefd045c1db1d7dd4ec4061efdd231f
Stinger101 <john.ngugi716@strathmore.edu>
2023-11-28 22:26:45 +0300

    added search

--- case_interview_app/templatetags/case_extras.py
+++ case_interview_app/templatetags/case_extras.py
@@ -23,11 +23,11 @@ def lang_url(url,language):
     return url
 
 @register.simple_tag
-def search(user):
+def search(request):
     searches = {}
     for s in Search.objects.all():
         if s.is_admin == 1:
-            if(user.is_staff):
+            if(request.user.is_staff):
                 searches[s.search_text] = {
                     "id":s.id,
                     "search_text": s.search_text,
@@ -36,7 +36,7 @@ def search(user):
                     "search_tag": s.search_tag.name
                 }
         elif s.is_technical == 1:
-            if(user.is_staff or user.has_perm):
+            if(request.user.is_staff or request.user.has_perm):
                 searches[s.search_text] = {
                     "id":s.id,
                     "search_text": s.search_text,
@@ -45,7 +45,7 @@ def search(user):
                     "search_tag": s.search_tag.name
                 }
         else:
-             if(user.is_staff or user.has_perm):
+             if(request.user.is_staff or request.user.has_perm):
                 searches[s.search_text] = {
                     "id":s.id,
                     "search_text": s.search_text,
@@ -58,50 +58,118 @@ def search(user):
     return mark_safe(json.dumps(searches))
 
 @register.filter()
-def search_get(user):
+def search_get(request):
     searches = []
-    if user.is_authenticated:
-        interviewer = Interviewer.objects.filter(email_address = user.email).first()
-    if user.is_authenticated:
+    if request.user.is_authenticated:
+        interviewer = Interviewer.objects.filter(email_address = request.user.email).first()
+    if request.user.is_authenticated:
         for s in Search.objects.all():
             if s.data_entry_purpose_id is not None:
                 if(interviewer.data_entry_purpose_id == s.data_entry_purpose_id):
-                    searches.append({
-                        "id":s.id,
-                        "search_text": s.search_text,
-                        "search_link": s.search_link,
-                        "search_description": s.search_description,
-                        "search_tag": s.search_tag.name
-                    })
+                    if request.LANGUAGE_CODE == "en":
+                        searches.append({
+                            "id":s.id,
+                            "search_text": s.search_text,
+                            "search_link": s.search_link,
+                            "search_description": s.search_description,
+                            "search_tag": s.search_tag.name
+                        })
+                    elif request.LANGUAGE_CODE == "fr":
+                        searches.append({
+                            "id":s.id,
+                            "search_text": s.search_text_fr,
+                            "search_link": s.search_link,
+                            "search_description": s.search_description_fr,
+                            "search_tag": s.search_tag.name
+                        })
+                    elif request.LANGUAGE_CODE == "pt":
+                        searches.append({
+                            "id":s.id,
+                            "search_text": s.search_text_pt,
+                            "search_link": s.search_link,
+                            "search_description": s.search_description_pt,
+                            "search_tag": s.search_tag.name
+                        })
                 else:
                     continue
             if s.is_admin == 1:
-                if(user.is_staff):
-                    searches.append({
-                        "id":s.id,
-                        "search_text": s.search_text,
-                        "search_link": s.search_link,
-                        "search_description": s.search_description,
-                        "search_tag": s.search_tag.name
-                    })
+                if(request.user.is_staff):
+                    if request.LANGUAGE_CODE == "en":
+                        searches.append({
+                            "id":s.id,
+                            "search_text": s.search_text,
+                            "search_link": s.search_link,
+                            "search_description": s.search_description,
+                            "search_tag": s.search_tag.name
+                        })
+                    elif request.LANGUAGE_CODE == "fr":
+                        searches.append({
+                            "id":s.id,
+                            "search_text": s.search_text_fr,
+                            "search_link": s.search_link,
+                            "search_description": s.search_description_fr,
+                            "search_tag": s.search_tag.name
+                        })
+                    elif request.LANGUAGE_CODE == "pt":
+                        searches.append({
+                            "id":s.id,
+                            "search_text": s.search_text_pt,
+                            "search_link": s.search_link,
+                            "search_description": s.search_description_pt,
+                            "search_tag": s.search_tag.name
+                        })
             elif s.is_technical == 1:
-                if(user.is_staff or user.has_perm):
-                    searches.append({
-                        "id":s.id,
-                        "search_text": s.search_text,
-                        "search_link": s.search_link,
-                        "search_description": s.search_description,
-                        "search_tag": s.search_tag.name
-                    })
+                if(request.user.is_staff or request.user.has_perm):
+                    if request.LANGUAGE_CODE == "en":
+                        searches.append({
+                            "id":s.id,
+                            "search_text": s.search_text,
+                            "search_link": s.search_link,
+                            "search_description": s.search_description,
+                            "search_tag": s.search_tag.name
+                        })
+                    elif request.LANGUAGE_CODE == "fr":
+                        searches.append({
+                            "id":s.id,
+                            "search_text": s.search_text_fr,
+                            "search_link": s.search_link,
+                            "search_description": s.search_description_fr,
+                            "search_tag": s.search_tag.name
+                        })
+                    elif request.LANGUAGE_CODE == "pt":
+                        searches.append({
+                            "id":s.id,
+                            "search_text": s.search_text_pt,
+                            "search_link": s.search_link,
+                            "search_description": s.search_description_pt,
+                            "search_tag": s.search_tag.name
+                        })
             else:
-                if(user.is_staff or user.has_perm):
-                    searches.append({
-                        "id":s.id,
-                        "search_text": s.search_text,
-                        "search_link": s.search_link,
-                        "search_description": s.search_description,
-                        "search_tag": s.search_tag.name
-                    })
+                if(request.user.is_staff or request.user.has_perm):
+                    if request.LANGUAGE_CODE == "en":
+                        searches.append({
+                            "id":s.id,
+                            "search_text": s.search_text,
+                            "search_link": s.search_link,
+                            "search_description": s.search_description,
+                            "search_tag": s.search_tag.name
+                        })
+                    elif request.LANGUAGE_CODE == "fr":
+                        searches.append({
+                            "id":s.id,
+                            "search_text": s.search_text_fr,
+                            "search_link": s.search_link,
+                            "search_description": s.search_description_fr,
+                            "search_tag": s.search_tag.name
+                        })
+                    elif request.LANGUAGE_CODE == "pt":
+                        searches.append({
+                            "id":s.id,
+                            "search_text": s.search_text_pt,
+                            "search_link": s.search_link,
+                            "search_description": s.search_description_pt,
+                            "search_tag": s.search_tag.name
+                        })
             
         
     return mark_safe(searches)
