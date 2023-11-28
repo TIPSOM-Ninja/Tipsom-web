72b22b8c9accd53d99b92ae35febfcb3f6929200
Stinger101 <john.ngugi716@strathmore.edu>
2023-11-28 15:29:11 +0300

    more robust host setting

--- tipnet/settings.py
+++ tipnet/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','34.90.216.227']
+ALLOWED_HOSTS = [env('DJANGO_HOST')]
 
 
 # Application definition
