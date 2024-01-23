110c142865ec69d76444cf842e67f30e14ca644f
Stinger101 <john.ngugi716@strathmore.edu>
2024-01-23 23:02:24 +0300

    added time to expire

--- tipnet/settings.py
+++ tipnet/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import environ
 import os
 from django.utils.translation import gettext_lazy as _
+from datetime import timedelta
 
 env = environ.Env()
 environ.Env.read_env()
