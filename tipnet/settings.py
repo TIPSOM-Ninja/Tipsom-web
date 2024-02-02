b6f681a3a0f851c187e67837140081f7c7d42dfb
Stinger101 <john.ngugi716@strathmore.edu>
2024-02-02 17:36:09 +0300

    added refresh timeline

--- tipnet/settings.py
+++ tipnet/settings.py
@@ -189,5 +189,5 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=365),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=2),
 }
