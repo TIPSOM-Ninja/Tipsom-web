97e8862f70e5f46982bf4d7f35765814bc8380be
Stinger101 <john.ngugi716@strathmore.edu>
2024-03-20 11:59:25 +0300

    bug fix som

--- case_interview_app/models.py
+++ case_interview_app/models.py
@@ -438,6 +438,7 @@ class Interviewer(models.Model):
     approval_comments = models.TextField( null=True, blank=True)
     victims = models.ManyToManyField(VictimProfile, null=True, blank=True)
     som_cases = models.ManyToManyField(SomCase, null=True, blank=True)
+    som_victims = models.ManyToManyField(SomVictimProfile, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True, null = True, blank = True)
     updated_at = models.DateTimeField(auto_now=True, null = True, blank = True)
 
