0a8429ce70b07c6e9d49f24342109e4099877b24
Stinger101 <john.ngugi716@strathmore.edu>
2024-04-08 15:47:29 +0300

    serializers victim profile

--- case_interview_app/apis.py
+++ case_interview_app/apis.py
@@ -153,21 +153,21 @@ class TipVictimAPIView(APIView):
             return Response({"error": "Victim not found"}, status=status.HTTP_404_NOT_FOUND)
 
         interviewer = Interviewer.objects.filter(email_address=request.user.email).first()
-        victim.citizenship_id = decrypt_data(request.data['citizenship'])
-        victim.countryOfBirth_id = decrypt_data(request.data['countryOfBirth'])
-        victim.gender_id = request.data['gender']
-        victim.race_id = decrypt_data(request.data['race'])
-        victim.place_of_birth = decrypt_data(request.data['placeOfBirth'])
-        victim.last_place_of_residence_id = decrypt_data(request.data['lastPlaceOfResidence'])
-        victim.identification_number = decrypt_data(request.data['idNumber'])
-        victim.initials = decrypt_data(request.data['initials'])
-        victim.age = decrypt_data(request.data['age'])
-        victim.address = decrypt_data(request.data['address'])
-        victim.email_address = decrypt_data(request.data['email'])
-        victim.interview_country_id = decrypt_data(request.data['interviewCountry'])
-        victim.interview_location = decrypt_data(request.data['interviewerLocation'])
-        victim.interview_date = decrypt_data(request.data['interviewDate'])
-        victim.additional_remarks = decrypt_data(request.data['additionalRemarks'])
+        victim.citizenship_id = decrypt_data(request.get('citizenship',victim.citizenship_id))
+        victim.countryOfBirth_id = decrypt_data(request.get('countryOfBirth',victim.countryOfBirth_id))
+        victim.gender_id = request.get('gender',victim.gender_id)
+        victim.race_id = decrypt_data(request.get('race',victim.race_id))
+        victim.place_of_birth = decrypt_data(request.get('placeOfBirth',victim.place_of_birth))
+        victim.last_place_of_residence_id = decrypt_data(request.get('lastPlaceOfResidence',victim.last_place_of_residence_id))
+        victim.identification_number = decrypt_data(request.get('idNumber',victim.identification_number))
+        victim.initials = decrypt_data(request.get('initials',victim.initials))
+        victim.age = decrypt_data(request.get('age',victim.age))
+        victim.address = decrypt_data(request.get('address',victim.address))
+        victim.email_address = decrypt_data(request.get('email',victim.email_address))
+        victim.interview_country_id = decrypt_data(request.get('interviewCountry',victim.interview_country_id))
+        victim.interview_location = decrypt_data(request.get('interviewerLocation',victim.interview_location))
+        victim.interview_date = decrypt_data(request.get('interviewDate',victim.interview_date))
+        victim.additional_remarks = decrypt_data(request.get('additionalRemarks',victim.additional_remarks))
         victim.approval_id = 1
         victim.consent_share_gov_patner = 1
         victim.consent_limited_disclosure = 1
@@ -373,7 +373,7 @@ class TipExploitationAPIView(APIView):
         elif(v_id is not None and pk is None):
             exploitation =Exploitation.objects.filter(victim_id = v_id)
             serializer = ExploitationSerializer(exploitation, many = True)
-            
+
         elif pk is not None:
             exploitation =Exploitation.objects.filter(victim_id = v_id).first()
             serializer = ExploitationSerializer(exploitation)
