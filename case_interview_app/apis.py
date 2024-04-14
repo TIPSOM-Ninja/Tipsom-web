39c79406b79dd526fb739a06f20593da050b1279
Stinger101 <john.ngugi716@strathmore.edu>
2024-04-14 03:24:36 +0300

    fixing bugs

--- case_interview_app/apis.py
+++ case_interview_app/apis.py
@@ -1471,7 +1471,7 @@ class SomArrestAPIView(APIView):
 
         return Response({"message": "Arrest details created successfully","id":arrest.id}, status=status.HTTP_201_CREATED)
 
-    def put(self, request, pk=None):
+    def put(self, request, v_id=None, pk=None):
         interviewer = Interviewer.objects.filter(email_address=request.user.email).first()
         arrest = SomArrestInvestigation.objects.filter(pk=pk).first()
         if not arrest:
