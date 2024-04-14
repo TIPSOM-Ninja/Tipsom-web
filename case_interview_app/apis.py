58dc25b5caaa6383efa4f67172bc253bedbf2cb8
Stinger101 <john.ngugi716@strathmore.edu>
2024-04-14 19:28:40 +0300

    fixing bugs

--- case_interview_app/apis.py
+++ case_interview_app/apis.py
@@ -780,7 +780,7 @@ class TipAssistanceAPIView(APIView):
 
         return Response({"message": "Arrest details created successfully","id":assistance.id}, status=status.HTTP_201_CREATED)
 
-    def put(self, request, pk=None):
+    def put(self, request, v_id = None, pk=None):
         interviewer = Interviewer.objects.filter(email_address=request.user.email).first()
         assistance = Assistance.objects.filter(pk=pk).first()
         if not assistance:
