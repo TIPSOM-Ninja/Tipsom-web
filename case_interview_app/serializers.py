a06e1fe840a1e89eca0412e5d4208b86b3a42b83
Stinger101 <john.ngugi716@strathmore.edu>
2024-04-14 05:32:58 +0300

    fixing bugs

--- case_interview_app/serializers.py
+++ case_interview_app/serializers.py
@@ -961,24 +961,24 @@ class VictimProfileWithRelatedSerializer(serializers.ModelSerializer):
     #     return obj.destinations.aggregate(Count('id', distinct=True))['id__count']
     
 class ProsecutionSerializer(serializers.ModelSerializer):
-    
+    trialCourt = TrialCourtSerializer(read_only=True)
+    suspectedTrafficker = SuspectedTraffickerSerializer(read_only=True)
+    prosecutionOutcome = ProsecutionOutcomeSerializer(read_only=True)
+    foreignCourtCountry = CountrySerializer(read_only=True)
+    caseStatus = CaseStatusSerializer(read_only=True)
+    verdict = VerdictSerializer(read_only=True)
+    penalty = SanctionPenaltySerializer(read_only=True)
+    acquitalReason = AquitalReasonSerializer(read_only=True)
+
     class Meta:
         model = Prosecution
         fields = ['id','suspectedTrafficker', 'courtCaseNumber', 'prosecutionOutcome', 'trialCourt','foreignCourtCountry', 'caseStatus', 'verdict','reviewAppealOutcome',  'penalty', 'guiltyVerdict','yearsImposed', 'acquitalReason','v_id']
         # Specify the field names in camelCase
         extra_kwargs = {
-            'suspectedTrafficker': {'source': 'trafficker_id'},
             'courtCaseNumber': {'source': 'court_case_no'},
-            'prosecutionOutcome': {'source': 'prosecution_outcome_id'},
-            'trialCourt': {'source': 'trial_court_id'},
-            'foreignCourtCountry': {'source': 'trial_court_country_id'},
-            'caseStatus': {'source': 'status_of_case_id'},
-            'verdict': {'source': 'verdict_id'},
             'reviewAppealOutcome': {'source': 'review_appeal_outcome'},
-            'penalty': {'source': 'sanction_penalty_id'},
             'guiltyVerdict': {'source': 'guilty_verdict'},
             'yearsImposed': {'source': 'years_imposed'},
-            'acquitalReason': {'source': 'aquital_reason_id'},
             'v_id': {'source':'victim_id'}
         }
 
